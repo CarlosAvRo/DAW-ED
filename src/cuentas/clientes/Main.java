23a73b15b1b6a372ff6c1761862a23e3e8860006
carlo <carlo@PORTATIL>
2024-02-14 20:23:54 +0100

    Inicial

---
+++ src/cuentas/clientes/Main.java
@@ -0,0 +1,33 @@
+package cuentas.clientes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = operativa_cuenta(null);
+        
+        System.out.println("El saldo actual es:"+ cuenta1.estado() );
+
+        try {
+            cuenta1.retirar(2300);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    private static Cuenta operativa_cuenta(float cantidad) {
+        Cuenta cuenta1;
+        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        return cuenta1;
+    }
+}
